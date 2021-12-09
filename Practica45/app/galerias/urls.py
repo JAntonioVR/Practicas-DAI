11a1b0f7959d8366914160cd3b2acfaeeac3cd97
JAntonioVR <juanantoniovr98@gmail.com>
2021-12-09 18:03:48 +0100

    Acabados añadir y lista de galerias

--- Practica45/app/galerias/urls.py
+++ Practica45/app/galerias/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('test_template', views.test_template, name="test_template"),
-    path('crear_galeria', views.crear_galeria, name="crear_galeria")
+    path('crear_galeria', views.crear_galeria, name="crear_galeria"),
+    path('consulta_galerias', views.consulta_galerias, name="consulta_galerias")
 ]
--- Practica45/app/galerias/views.py
+++ Practica45/app/galerias/views.py
@@ -2,7 +2,7 @@
 
 from django.shortcuts import render, HttpResponse
 from django.views.generic import CreateView
-from .models import GaleriaForm, Cuadro
+from .models import Galeria, GaleriaForm, Cuadro
 # Create your views here.
 
 def index(request):
@@ -10,15 +10,29 @@ def index(request):
 
 def test_template(request):
     context = {}   # Aquí van la las variables para la plantilla
-    return render(request,'galerias/index.html', context)
+    return render(request,'index.html', context)
 
 def crear_galeria(request):
-
     form = GaleriaForm()
+    state = 0
+    errors = []
+    messages = []
     if request.method == 'POST':
-        '''
-        form = GaleriaForm(request.POST, request.FILES)
+        form = GaleriaForm(request.POST)
         if form.is_valid():
             form.save()
-        '''
-    return render(request, 'galerias/galeria_form.html', {'form': form})
+            messages.append("La galeria se ha guardado con éxito.")
+            state = 1
+        else:
+            errors.append("Los datos introducidos no eran válidos.")
+            state = -1
+    return render(request, 'galeria_form.html', { 
+        'form': form, 
+        'errors': errors, 
+        'messages': messages,
+        'state': state
+        })
+
+def consulta_galerias(request):
+    galerias = Galeria.objects.all()
+    return render(request, 'lista_galeria.html', {'galerias': galerias})
