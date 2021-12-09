420f1e60d9ef77078c0f1d2e7644978fd86dc196
JAntonioVR <juanantoniovr98@gmail.com>
2021-12-09 19:44:54 +0100

    Acabada la parte de galerias

--- Practica45/app/galerias/urls.py
+++ Practica45/app/galerias/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('test_template', views.test_template, name="test_template"),
     path('crear_galeria', views.crear_galeria, name="crear_galeria"),
-    path('consulta_galerias', views.consulta_galerias, name="consulta_galerias")
+    path('consulta_galerias', views.consulta_galerias, name="consulta_galerias"),
+    path('modificar_galeria/<int:pk>', views.modificar_galeria, name="modificar_galeria"),
+    path('eliminar_galeria/<int:pk>', views.eliminar_galeria, name="eliminar_galeria"),
+    
 ]
--- Practica45/app/galerias/views.py
+++ Practica45/app/galerias/views.py
@@ -26,6 +26,8 @@ def crear_galeria(request):
         else:
             errors.append("Los datos introducidos no eran válidos.")
             state = -1
+    else:
+        messages.append("Introduce los datos de la nueva galería:")
     return render(request, 'galeria_form.html', { 
         'form': form, 
         'errors': errors, 
@@ -36,3 +38,41 @@ def crear_galeria(request):
 def consulta_galerias(request):
     galerias = Galeria.objects.all()
     return render(request, 'lista_galeria.html', {'galerias': galerias})
+
+def modificar_galeria(request, pk):
+    galeria = Galeria.objects.get(pk=pk)
+    form = GaleriaForm(instance=galeria)
+    state = 0
+    errors = []
+    messages = []
+    if request.method == 'POST':
+        form = GaleriaForm(request.POST, instance=galeria)
+        if form.is_valid():
+            form.save()
+            messages.append("La galeria se ha modificado con éxito.")
+            state = 1
+        else:
+            errors.append("Los datos introducidos no eran válidos.")
+            state = -1
+    return render(request, 'modificar_galeria.html', { 
+        'form': form, 
+        'errors': errors, 
+        'messages': messages,
+        'state': state,
+        'pk' : pk
+        })
+
+def eliminar_galeria(request, pk):
+    galeria = Galeria.objects.get(pk=pk)
+    nombre_galeria = galeria.nombre
+    borrado = galeria.delete()
+    errors = []
+    messages = []
+    if borrado[0] > 0:
+        messages.append("Se ha eliminado la siguiente galeria: " + nombre_galeria)
+    else:
+        errors.append("Ha ocurrido algún error en el borrado")
+    return render(request, 'eliminar_galeria.html', {
+        'errors': errors,
+        'messages': messages,
+    })
